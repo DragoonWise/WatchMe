58ca775b5f6654a19dcd9177979cb81e38e29917
DragoonWise <dragoonwise@hotmail.com>
2020-03-23 13:40:01 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit b3c09bfa5c4ba3c0583d4df430c4505fe3ddd931, reversing
    changes made to 67a38e14baf03752bbda79a6424aef57cd4139a0.

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/admin', 'AdminController@dashboard');
+Route::get('/admin/users/{page?}', 'AdminController@users');
+Route::get('/admin/user/{id}', 'AdminController@user');
+
+Route::get('/', function () {
+    return view('welcome');
+});
