7212bc98d0835ab4eda4775ce7c3566fbd19c900
Cleyam <cleyam@gmail.com>
2020-04-06 09:06:34 +0200

    register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,8 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Movie;
+use App\Http\API\ImageHelper;
 
 class RegisterController extends Controller
 {
